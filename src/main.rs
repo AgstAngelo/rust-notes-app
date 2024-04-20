dbe4ba21bf4e3b847ed3edd97d60dce96d57329f
Augusto Angelo <agstangelo@gmail.com>
2024-04-20 13:58:10 -0300

    Feat: First commit with axum + sqlx (just getting a server running and a simple query)

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use axum::{ routing::get, Router };
+use sqlx::postgres::PgPoolOptions;
+use dotenv::dotenv;
+use sqlx::Row;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+    let db_str = std::env::var("DATABASE_URL").expect("database config not found");
+    let app_str = std::env::var("APP_STR").expect("app config not found");
+    
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_str)
+        .await.expect("couldn't connect");
+
+    let app = Router::new().route("/", get(handler));
+    let listener = tokio::net::TcpListener::bind(&app_str).await.unwrap();
+   
+    let res = sqlx::query("SELECT 1 + 1 as sum").fetch_one(&pool).await.expect("couldn't complete query");
+    let sum: i32 = res.get("sum");
+    println!("1+1={}", sum); 
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+   "Hello, World!"
+}
